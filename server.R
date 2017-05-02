b935ea492357424a42898f26df00bb932fde5897
zefeng-zhang <zzhang107@dons.usfca.edu>
2017-05-01 23:13:36 -0700

    formatting

--- server.R
+++ server.R
@@ -1,20 +1,24 @@
 library(shiny)
 library(ggplot2)
+library(wordcloud)
 
 function(input, output) {
   
-  # choose columns to display
+  ################
+  # Flight Table #
+  ################
   output$mytable1 <- DT::renderDataTable({
-     origin_dest_agg<-origin_dest_agg[origin_dest_agg$origin_state %in% c(input$show_vars2), ]
-    
+    origin_dest_agg <- origin_dest_agg[origin_dest_agg$origin_state %in% c(input$show_vars2) &
+                                       origin_dest_agg$dest_state %in% c(input$show_vars2), ]
     DT::datatable(origin_dest_agg[, input$show_vars, drop = FALSE])
   })
   
-  # sorted columns are colored now because CSS are attached to them
+  #################
+  # Force Network #
+  #################
   output$NetworkPlot <- renderForceNetwork({
-    
-    origin_dest_agg<-origin_dest_agg[origin_dest_agg$origin_state %in% c(input$show_vars2), ]
-    
+    origin_dest_agg <- origin_dest_agg[origin_dest_agg$origin_state %in% c(input$show_vars2) &
+                                       origin_dest_agg$dest_state %in% c(input$show_vars2), ]
     # Prepare NODES dataframe
     cities <- c(origin_dest_agg$origin_city, origin_dest_agg$dest_city)
     nodes <- data.frame(cities)
@@ -28,39 +32,30 @@ function(input, output) {
     links$origin_city <- as.integer(links$origin_city) - 1
     links$dest_city <- factor(links$dest_city, levels=citie_levels)
     links$dest_city <- as.integer(links$dest_city) - 1
-    links$count <- links$count/max(links$count) *1
-    
-  
+    links$count <- links$count/max(links$count) * 20.0
     
     forceNetwork(Links = links, Nodes = nodes,
                  Source = "origin_city", Target = "dest_city",
                  Value = "count", NodeID = "cities",
-                 Group = "states", opacity = 0.9, legend = TRUE, zoom = TRUE,bounded=TRUE)
+                 Group = "states", opacity = 0.9, legend = TRUE, zoom = TRUE, bounded=TRUE)
   })
-  
-  # customize the length drop-down menu; display 5 rows per page by default
-  output$WordMap <- renderPlot({
-    library(wordcloud)
 
-    #Create a list of words
-    
-    
-    origin_dest_agg<-origin_dest_agg[origin_dest_agg$origin_state %in% c(input$show_vars2), ]
-    
+  ############
+  # Word Map #
+  ############
+  output$WordMap <- renderPlot({
+    raw_origin_dest <- subset(raw_data, ,select=c("ORIGIN_CITY_NAME", "DEST_CITY_NAME"))
+    raw_origin_dest$ORIGIN_CITY_NAME <- as.character(raw_origin_dest$ORIGIN_CITY_NAME)
+    origin_separate <- separate(raw_origin_dest, ORIGIN_CITY_NAME, into = c("origin_city", "origin_state"), sep = ", ")
+    origin_separate <- origin_separate[origin_separate$origin_state %in% c(input$show_vars2), ]
     origin_agg <- data.frame(table(origin_dest_agg$origin_city))
     a = origin_agg$Var1
-
-    #I give a frequency to each word of this list
     b = origin_agg$Freq
 
-    #The package will automatically make the wordcloud ! (I add a white background)
     par(bg="white")
-    pal2<-brewer.pal(8,'Dark2')
-    wordcloud(a , b , rot.per=0.1,colors=pal2,
-              alpha=0.8,use.r.layout = FALSE)
-    
-    
-  
+    pal2 <- brewer.pal(8,'Dark2')
+    wordcloud(a , b , rot.per=0.1, colors=pal2,
+              alpha=0.8, use.r.layout = FALSE)
   })
   
 }
--- ui.R
+++ ui.R
@@ -8,28 +8,22 @@ fluidPage(
     'input.dataset === "Flight Table"',
     checkboxGroupInput('show_vars', 'Columns in Flights to show:',
                        names(origin_dest_agg), 
-                       selected = names(origin_dest_agg))
+                       selected = c("NY", "CA", "TX"))
   ),
-
-      conditionalPanel(
-        'input.dataset === "iris"',
-        helpText('Display 5 records by default.')
-      ),
   
   sidebarLayout(
     sidebarPanel(
       checkboxGroupInput('show_vars2', 'Flight Origin by State:',
                          sort(unique(origin_dest_agg$dest_state)),
-                         selected = c('MA','AK'))
+                         selected = c('NY','CA', 'TX'))
     ),
     mainPanel(
       tabsetPanel(
         id = 'dataset',
         tabPanel('Flight Table', DT::dataTableOutput('mytable1')),
-        tabPanel('NetworkPlot', forceNetworkOutput("NetworkPlot",height='680px')),
-        tabPanel('WordMap', plotOutput('WordMap',height = '680px'))
-      )
-      )     
+        tabPanel('NetworkPlot', forceNetworkOutput("NetworkPlot", height='680px')),
+        tabPanel('WordMap', plotOutput('WordMap', height = '680px'))
+        ))
     )
   )
  
