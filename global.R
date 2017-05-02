3e58d83aa362b0a2d2f3df0cdecd4f15211881ad
zefeng-zhang <zzhang107@dons.usfca.edu>
2017-05-02 00:43:13 -0700

    add dendro

--- global.R
+++ global.R
@@ -24,18 +24,3 @@ origin_dest_agg <- unique(origin_dest_agg[, 1:3])
 # Separate cities and states
 origin_dest_agg <- separate(origin_dest_agg, origin_city, into = c("origin_city", "origin_state"), sep = ", ")
 origin_dest_agg <- separate(origin_dest_agg, dest_city, into = c("dest_city", "dest_state"), sep = ", ")
-
-
-temp <- origin_dest_agg[origin_dest_agg$origin_state %in% c('NY', 'CA') &
-                          origin_dest_agg$dest_state %in% c('NY', 'CA'), ]
-cities <- c(temp$origin_city, temp$dest_city)
-cities <- sort(unique(cities))
-m <- matrix(0, ncol = length(cities), nrow = length(cities))
-m <- data.frame(m)
-rownames(m) <- cities
-colnames(m) <- cities
-for (i in seq(1, nrow(temp))){
-  row <- temp[i, ]
-  m[row$origin_city, row$dest_city] = row$count
-  m[row$dest_city, row$origin_city] = row$count
-}
